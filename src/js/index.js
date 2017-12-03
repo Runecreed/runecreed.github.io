73905e7d5149925647d8114006daedb176ed26d0
Runecreed <kevin.cleijne@live.nl>
2017-12-03 17:25:45 +0100

    Experimentation with google maps API, transitions, Jquery

--- src/js/index.js
+++ src/js/index.js
@@ -4,10 +4,12 @@ import $ from 'jquery'; //  Redundant but good for esLint support
 import './plugins.js'; // Load jquery plugins
 import 'materialize-css/dist/js/materialize.min.js';
 import 'materialize-css/dist/css/materialize.min.css';
-
+import '../css/style.css';
+import loadGoogleMapsApi from 'load-google-maps-api-2';
 
 //  Import images
 import panda from '../img/panda.gif';
+
 
 function addImage() {
     let $body = $("body");
@@ -31,13 +33,83 @@ function getImageURL(input) {
     }
 }
 
-$(document).ready(function (e) {
-    $("#imgInp").change(function (selection) {
-        getImageURL(this);
+
+var map;
+
+function handleNoGeolocation(errorFlag, googleMap) {
+    if (errorFlag) {
+        var content = 'Error: The Geolocation service failed.';
+    } else {
+        var content = 'Error: Your browser doesn\'t support geolocation.';
+    }
+
+    var options = {
+        map: map,
+        position: new googleMap.LatLng(60, 105),
+        content: content
+    };
+
+    var infowindow = new googleMap.InfoWindow(options);
+    map.setCenter(options.position);
+}
+
+function initialize(googleMap) {
+    var mapOptions = {
+        zoom: 15,
+        mapTypeId: googleMap.MapTypeId.ROADMAP
+    };
+
+    map = new googleMap.Map($('#map-canvas')[0],
+        mapOptions);
+
+    // try HTML5 geolocation
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var pos = new googleMap.LatLng(position.coords.latitude,
+                position.coords.longitude);
+
+            var infowindow = new googleMap.InfoWindow({
+                map: map,
+                position: pos,
+                content: 'Location found using HTML5.'
+            });
+            map.setCenter(pos);
+        }, function () {
+            handleNoGeolocation(true, googleMap);
+        });
+    } else {
+        // browser doesn't support geolocation
+        handleNoGeolocation(false, googleMap);
+    }
+}
+
+
+loadGoogleMapsApi({'key': 'AIzaSyATSH5MpScAOz-t1vPrAFMuqqhKU5RsvaQ'}).then(function (googleMap) {
+    console.log(googleMap); // => Object { Animation: Object, ...
+    // When promise is returned, initialize the map if the browser is ready.
+    googleMap.event.addDomListener(window, "load", initialize(googleMap));
+}).catch(function (err) {
+    console.error(err);
+});
+
+
+function enhance(target) {
+    let $target = $(target);
+    console.log($target);
+    $target.addClass("scale-out").delay(1000).queue(() => {
+        $target.removeClass("scale-out").dequeue();
     });
+}
+
+$(document).ready(function (e) {
     addImage();
 
     $('#modal1').modal('open');
+
+    $('#enhance').click((event) => {
+        enhance(event.target);
+    });
+
 });
 
 // addImage('./src/img/panda.gif');
